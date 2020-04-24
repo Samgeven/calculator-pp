51787d41e3ad39553107c800e421da357fd7da5e
unknown <sam.geven@mail.ru>
2020-04-24 18:47:48 +0300

    Сделал все поля, добавил анимацию

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,42 @@
-console.log('it works!');
+// services
 $(document).ready(function() {
-    $('.btn_nav').click(function() {
-        let currentTab = '1';
-        if (this.dataset.goto == 'next') {
-            currentTab++
-            $('.calc-tab-item').dataset.order == currentTab ? $('.calc-tab-item').dataset.order
+	$('.calc-form-services__item-wrp').click(function() {
+		$(this).toggleClass('active');
+	});
+});
+
+//forms
+
+var form = $("#calc-form");
+form.validate({
+    errorPlacement: function errorPlacement(error, element) { element.before(error); },
+    rules: {
+        confirm: {
+            equalTo: "#password"
         }
-    });
+    }
+});
+form.children("div").steps({
+    headerTag: "p",
+    bodyTag: "section",
+    transitionEffect: "fade",
+    transitionEffectSpeed: "400",
+	labels: {
+	    next: "Дальше",
+	    previous: "Назад"
+	},
+    onStepChanging: function (event, currentIndex, newIndex)
+    {
+        form.validate().settings.ignore = ":disabled,:hidden";
+        return form.valid();
+    },
+    onFinishing: function (event, currentIndex)
+    {
+        form.validate().settings.ignore = ":disabled";
+        return form.valid();
+    },
+    onFinished: function (event, currentIndex)
+    {
+        alert("Submitted!");
+    }
 });
